ce1ae4814766bbac65fc70ce6a821cab5d252397
majoaavila <mavila7@ucol.mx>
2022-05-19 08:19:51 -0500

    practica20

---
+++ routes/persons.js
@@ -0,0 +1,34 @@
+const express = require('express'); // se inyecta la dependencia
+const router = express.Router(); // generamos la instancia del router
+const mongoose = require('../node_modules/mongoose'); // se inyecta la dependencia de mongoose
+
+let Person = require('../models/person'); //inyecta dependencia del modelo person
+
+//se agrega ruta persons => metodo GET
+router.get('/persons', function (req, res, next) {
+    Person.find(function (err, persons) {
+        if(err) return next(err);
+        res.json(persons);
+    });
+});
+
+//ruta GET - permite mostrar el formulario en pantalla para insertar los datos a nuestra database, aqui se renderiza la vista del form en html
+router.get('/person', function(req, res) {
+    res.render('person');
+});
+
+
+//ruta POST - atiende la peticion, aqui agregamos la ruta POST, para poder agregar un nuevo documento a la coleccion
+router.post('/addPerson', function(req, res) {
+    //creamos la entidad - toma como valores los que son introducidos al body de la pericion
+    const myPerson = new Person ({
+        nombre: req.body.nombre,
+        edad: req.body.edad,
+        tipoSangre: req.body.tipoSangre,
+        nss: req.body.nss });
+    //se guarda en la database
+    myPerson.save();
+});
+
+//exportar el router
+module.exports = router;
